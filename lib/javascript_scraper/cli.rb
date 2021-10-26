3d655a8ea41db434b1e1954b769068c9b961c221
Jason <jason.tong0910@outlook.com>
2021-10-26 07:33:46 -0400

    Added instruction class with 3 methods in CLI

--- lib/javascript_scraper/cli.rb
+++ lib/javascript_scraper/cli.rb
@@ -1,5 +1,28 @@
-class JavascriptScaper::CLI
-    def call
-        puts 'hey!'
+
+
+module JavascriptScraper
+    class CLI
+        def call
+            puts 'HELLO!'
+        end
     end
+end
+
+class Instructions
+    def introductions
+        puts 'Welcome to dev.to websraper. This CLI tool gathers aricles based on the hashtag provided.'
+        puts 'If you want to quit, simply type (q) the next time you are prompted to enter a value.'
+        puts 'Please provide a hashtag to continue.'
+        puts ''
+    end
+
+    def quit_message
+        puts 'You have quit the webscraper.'
+    end
+
+    def invalid_entry
+        puts 'Invalid entry, try again.'
+    end
+
+    
 end
