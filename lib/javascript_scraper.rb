802624aedfb9e5e3f4b5bddeb35619a9c3265f3a
Jason <jason.tong0910@outlook.com>
2021-10-26 12:04:28 -0400

    Added scraper class to javascript_scraper.rb, but not working yet

--- lib/javascript_scraper.rb
+++ lib/javascript_scraper.rb
@@ -5,3 +5,28 @@ module JavascriptScraper
   class Error < StandardError; end
   # Your code goes here...
 end
+
+
+class Scraper 
+  extend Instructions
+
+  def self.get_input
+    if user_input == 'q'
+      quit_message
+    elsif user_input.empy?
+      invalid_entry
+      get_input
+    else
+      scrape_data(user_input.to_s)
+    end
+  end
+
+  def self.scrape_data(hashtag)
+    puts 'Scraped data for #{hashtag}'
+    get_input
+  end
+end
+
+Scraper.introductions
+Scraper.get_input
+
